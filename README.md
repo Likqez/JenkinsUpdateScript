f5e411be6a1854a8a7a3d5c68076419ff99648c2
Lukas Lanzner <lanzner09@gmail.com>
2020-11-23 18:11:00 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+#JenkinsUpdateScript
+A script for updating a _**dockerized**_ Jenkins installation, just by the ``CONTAINER ID``
---
+++ update.sh
@@ -0,0 +1,26 @@
+echo -n "Please enter your Jenkins Container ID: "
+read id
+
+docker exec -u root $id wget --directory-prefix=/home/jenkins/ https://updates.jenkins.io/latest/jenkins.war
+docker exec -u root $id mv home/jenkins/jenkins.war /usr/share/jenkins
+docker exec -u root $id chown jenkins:jenkins /usr/share/jenkins/jenkins.war
+
+read -p "New 'jenkins.war' was successfully deployed. Do you want to restart Jenkins now? (yes/no) " reply
+
+choice=$(echo $reply|sed 's/(.*)/L1/')
+
+if [ "$choice" = 'yes' ]
+then
+  echo "Restarting Container.."
+  docker restart $id;
+
+elif [ "$choice" = 'no'  ]
+then
+
+  echo "You selected 'no', exiting in 3 seconds";
+  echo "Keep in mind you have to restart jenkins manually :)"
+  sleep 3
+  exit 0
+  else
+echo "invalid answer, type yes or no";
+fi
